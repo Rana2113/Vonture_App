7e97964f41b1674180850896f3b56ecda0b7a828
Rana Mustafa <rna0882@gmail.com>
2024-05-29 18:13:14 +0300

    - Splash View Done

---
+++ lib/core/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xffFFFBF8);
+const kButtonColor = Color(0xffF5C499);
+const kTextColor = Color(0xff877563);
+const kBorderColor2 = Color(0xffE8DBD1);
+const kFocusBorder = Color(0xff96734F);
+const kErrorBorder = Color(0xff5B340D);
+const kfillColor = Color(0xffFBF2EC);
